7553f4cb85ea686b9f783b96d65be443a245038c
phucpercy <phucpercy@gmail.com>
2019-12-21 16:41:27 +0700

    [Fix Bugs] Remove social network login

--- app/auth/authentic.js
+++ app/auth/authentic.js
@@ -1,23 +1,11 @@
-'use strict';
-
-var config 		= require('../config');
 var passport 	= require('passport');
-var logger 		= require('../logger');
 
-var LocalStrategy 		= require('passport-local').Strategy;
-var FacebookStrategy  	= require('passport-facebook').Strategy;
-var TwitterStrategy  	= require('passport-twitter').Strategy;
+var localStrategy 		= require('passport-local').Strategy;
 
 var User = require('../models/user');
 
-/**
- * Encapsulates all code for authentication 
- * Either by using username and password, or by using social accounts
- *
- */
 var init = function(){
 
-	// Serialize and Deserialize user instances to and from the session.
 	passport.serializeUser(function(user, done) {
 		done(null, user.id);
 	});
@@ -28,8 +16,7 @@ var init = function(){
 		});
 	});
 
-	// Plug-in Local Strategy
-	passport.use(new LocalStrategy(
+	passport.use(new localStrategy(
 	  function(username, password, done) {
 	    User.findOne({ username: new RegExp(username, 'i'), socialId: null }, function(err, user) {
 	      if (err) { return done(err); }
@@ -49,19 +36,6 @@ var init = function(){
 	    });
 	  }
 	));
-
-	// In case of Facebook, tokenA is the access token, while tokenB is the refersh token.
-	// In case of Twitter, tokenA is the token, whilet tokenB is the tokenSecret.
-	var verifySocialAccount = function(tokenA, tokenB, data, done) {
-		User.findOrCreate(data, function (err, user) {
-	      	if (err) { return done(err); }
-			return done(err, user); 
-		});
-	};
-
-	// Plug-in Facebook & Twitter Strategies
-	passport.use(new FacebookStrategy(config.facebook, verifySocialAccount));
-	passport.use(new TwitterStrategy(config.twitter, verifySocialAccount));
 
 	return passport;
 }
